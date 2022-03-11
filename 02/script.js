505273a4d9e2675bf7abf42ccfb9bfdbe107c852
Kiran Ashok Bansode <kiranbansode>
2022-03-11 16:43:10 +0530

    2.23. The Conditional (Ternary) Operator

--- 02/script.js
+++ 02/script.js
@@ -286,13 +286,32 @@ console.log(lastName); */
 // 	console.log("Not a valid day");
 // }
 
-3 + 4;
-1991;
-true && false && !false;
+// 3 + 4;
+// 1991;
+// true && false && !false;
 
-if (23 > 10) {
-	const str = "23 is bigger";
+// if (23 > 10) {
+// 	const str = "23 is bigger";
+// }
+
+// const me = "kiran";
+// console.log(`I'm ${2037 - 1991} years old ${me}`);
+
+const age = 26;
+age >= 18
+	? console.log("I like to drink wine 🍷 ")
+	: console.log("I like to drink water 💧 ");
+
+const drink = age >= 18 ? "Wine 🍷" : "Water 💧";
+
+let drink2;
+if (age >= 18) {
+	drink2 = "Wine 🍷";
+} else {
+	drink2 = "Water 💧";
 }
 
-const me = "kiran";
-console.log(`I'm ${2037 - 1991} years old ${me}`);
+console.log(drink);
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "Wine 🍷" : "Water 💧"}`);
