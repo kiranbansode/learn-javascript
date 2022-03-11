b4f67aa5d19ede49621eb2104e0c59f74dd66889
Kiran Ashok Bansode <kiranbansode>
2022-03-11 11:21:25 +0530

    2.05. Values and Variables

--- 02/script.js
+++ 02/script.js
@@ -1,4 +1,27 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is Fun");
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("Kiran");
+console.log(23);
+
+// Declaring a variable name firstName with the help of `let`
+let firstName = "Meera";
+
+//to access stored value in variable just use variable name
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let kiran_bansode = "KB";
+let $function = 27;
+
+let person = "kiran";
+let PI = 3.1415;
+
+let myFirstJob = "Teacher";
+let myFutureJob = "Software Engineer";
+
+let job1 = "techer";
+let job2 = "programmer";
+
+console.log(myFutureJob);
