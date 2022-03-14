4b7fb70d7cd6da7a1a9ec33dfd8f99dc72cf39fc
Kiran Ashok Bansode <kiranbansode>
2022-03-14 11:13:10 +0530

    3.03 Functions

--- 03/script.js
+++ 03/script.js
@@ -1,10 +1,32 @@
 "use strict";
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive");
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can drive");
 
 // const interface = "Audio";
 // const private = 534;
+
+function logger() {
+	console.log("My name is Kiran");
+}
+
+// Invoking, Calling or Running function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+	const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+	return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+const num = Number("23");
