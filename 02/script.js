70c9e2e7633f0534c4e970f0f61f20c3bbf72ceb
Kiran Ashok Bansode <kiranbansode>
2022-04-17 18:00:56 +0530

    2.07. Data Types

--- 02/script.js
+++ 02/script.js
@@ -1,18 +1,19 @@
-let js = "amazing";
+/* let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 console.log("Kiran");
-console.log(23);
+console.log(23); */
 
 // Declaring a variable name firstName with the help of `let`
-let firstName = "Meera";
+// let firstName = "Meera";
 
 //to access stored value in variable just use variable name
+/* console.log(firstName);
 console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+console.log(firstName); */
 
-let kiran_bansode = "KB";
+// Variable name conventions
+/* let kiran_bansode = "KB";
 let $function = 27;
 
 let person = "kiran";
@@ -24,4 +25,30 @@ let myFutureJob = "Software Engineer";
 let job1 = "techer";
 let job2 = "programmer";
 
-console.log(myFutureJob);
+console.log(myFutureJob); */
+
+// Single Line Comment
+
+/* 
+    Multi Line Comment
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof "Kiran");
+
+javascriptIsFun = "YES";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
