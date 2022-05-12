baccd602e34046d522a35eb26bf15e4bf0ac91e2
Kiran Ashok Bansode <kiranbansode>
2022-05-12 23:10:25 +0530

    2.14. Coding Challenge #2

--- 02/Coding Challenges.js
+++ 02/Coding Challenges.js
@@ -1,5 +1,4 @@
 /* Coding Challenge 1 */
-
 const markHeight = 1.69;
 const johnHeight = 1.88;
 const markWeight = 78;
@@ -10,4 +9,10 @@ const johnBMI = johnWeight / johnHeight ** 2;
 
 const markHigherBMI = markBMI > johnBMI;
 
-console.log(markHigherBMI);
+/* Coding Challenge 2 */
+
+if (markBMI > johnBMI) {
+	console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})`);
+} else {
+	console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})`);
+}
