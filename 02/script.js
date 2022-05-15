8843542b572d4a9227ec5e7e8dc5ab64524b1bcf
Kiran Ashok Bansode <kiranbansode>
2022-05-15 16:47:08 +0530

    2.15. Type Conversion and Coercion

--- 02/script.js
+++ 02/script.js
@@ -139,22 +139,44 @@ console.log(lastName); */
 // multiple
 // lines`);
 
-const age = 15;
+// const age = 15;
 
-// control structure
-if (age >= 18) {
-	console.log(`Sarah can start driving license 🚗`);
-} else {
-	const yearsLeft = 18 - age;
-	console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
-}
+// // control structure
+// if (age >= 18) {
+// 	console.log(`Sarah can start driving license 🚗`);
+// } else {
+// 	const yearsLeft = 18 - age;
+// 	console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+// }
 
-const birthYear = 1991;
-let century;
-if (birthYear <= 2000) {
-	century = 20;
-} else {
-	century = 21;
-}
+// const birthYear = 1991;
+// let century;
+// if (birthYear <= 2000) {
+// 	century = 20;
+// } else {
+// 	century = 21;
+// }
 
-console.log(century);
+// console.log(century);
+
+// Type Conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Kiran"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// Type Coercion
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log(3 + "23" + "10");
+
+console.log("23" * "2");
+console.log("23" / "2");
+
+let n = "1" + 1; //'11'
+n = n - 1;
+console.log(n);
