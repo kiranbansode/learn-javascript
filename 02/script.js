f95eda1dc04c15429aeaa4e806350f5d91e02682
Kiran Ashok Bansode <kiranbansode>
2022-05-15 17:13:27 +0530

    2.16. Truthy and Falsy Values

--- 02/script.js
+++ 02/script.js
@@ -160,23 +160,44 @@ console.log(lastName); */
 // console.log(century);
 
 // Type Conversion
-const inputYear = "1991";
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
 
-console.log(Number("Kiran"));
-console.log(typeof NaN);
+// console.log(Number("Kiran"));
+// console.log(typeof NaN);
 
-console.log(String(23), 23);
+// console.log(String(23), 23);
 
-// Type Coercion
-console.log("I am " + 23 + " years old");
-console.log("23" - "10" - 3);
-console.log(3 + "23" + "10");
+// // Type Coercion
+// console.log("I am " + 23 + " years old");
+// console.log("23" - "10" - 3);
+// console.log(3 + "23" + "10");
 
-console.log("23" * "2");
-console.log("23" / "2");
+// console.log("23" * "2");
+// console.log("23" / "2");
 
-let n = "1" + 1; //'11'
-n = n - 1;
-console.log(n);
+// let n = "1" + 1; //'11'
+// n = n - 1;
+// console.log(n);
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Kiran"));
+console.log(Boolean(""));
+console.log(Boolean({}));
+console.log(Boolean([]));
+
+const money = 1000;
+if (money) {
+	console.log("Don't spend it all");
+} else {
+	console.log("You should get a job!");
+}
+
+let height = 0;
+if (height) {
+	console.log("YAY! Height is defined");
+} else {
+	console.log("Height is UNDEFINED");
+}
