63236ba735e162b4cef33defade5e2f751f8d11a
Kiran Ashok Bansode <kiranbansode>
2022-05-22 17:06:23 +0530

    2.19. Logical Operators

--- 02/script.js
+++ 02/script.js
@@ -202,24 +202,46 @@ console.log(lastName); */
 // 	console.log("Height is UNDEFINED");
 // }
 
-const age = "18";
-if (age === 18) console.log("You just became an adult :D");
+// const age = "18";
+// if (age === 18) console.log("You just became an adult :D");
 
-if (age == 18) console.log("You just became an adult :D (loosly)");
+// if (age == 18) console.log("You just became an adult :D (loosly)");
 
-const favourite = Number(prompt("What's your favourite number ?"));
+// const favourite = Number(prompt("What's your favourite number ?"));
 
-console.log(favourite);
-console.log(typeof favourite);
+// console.log(favourite);
+// console.log(typeof favourite);
 
-if (favourite === 23) {
-	console.log("Cool! 23 is an amazing number!");
-} else if (favourite === 7) {
-	console.log("7 is also a cool number");
-} else if (favourite === 9) {
-	console.log("9 is also a cool number");
+// if (favourite === 23) {
+// 	console.log("Cool! 23 is an amazing number!");
+// } else if (favourite === 7) {
+// 	console.log("7 is also a cool number");
+// } else if (favourite === 9) {
+// 	console.log("9 is also a cool number");
+// } else {
+// 	console.log("Number is not 23 or 7 or 9");
+// }
+
+// if (favourite !== 23) console.log("Why not 23 ?");
+
+const hasDriversLicense = true; //A
+const hasGoodVision = true; //B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if (hasDriversLicense && hasGoodVision) {
+// 	console.log("Sarah is able to drive");
+// } else {
+// 	console.log("Someone else should drive");
+// }
+
+const isTired = false; //C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+	console.log("Sarah is able to drive");
 } else {
-	console.log("Number is not 23 or 7 or 9");
+	console.log("Someone else should drive");
 }
-
-if (favourite !== 23) console.log("Why not 23 ?");
