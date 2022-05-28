e4816f28eca0e312ff7cbe8e4d13471ff1ff900d
kiranbansode <kiran5120135@gmail.com>
2022-05-28 12:05:17 +0530

    5.04. Installing Node.js and Setting Up a Dev Environment

--- 05/script.js
+++ 05/script.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const x = 23;
-if (x === 23) console.log(23);
 
-const calcAge = birthyear => 2022 - birthYear;
+const calcAge = birthYear => 2022 - birthYear;
+
+console.log(calcAge(1995));
