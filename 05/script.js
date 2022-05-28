f738a76d7240fcdd901319bd6e75b436015706ce
kiranbansode <kiran5120135@gmail.com>
2022-05-28 13:04:50 +0530

    5.07. Using Google, StackOverflow and MDN

--- 05/script.js
+++ 05/script.js
@@ -1,8 +1,70 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 23;
+// const x = 23;
 
-const calcAge = birthYear => 2022 - birthYear;
+// const calcAge = birthYear => 2022 - birthYear;
 
-console.log(calcAge(1995));
+// console.log(calcAge(1995));
+
+// Problem 1
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem
+// - What is temp amplitude ?
+// -> Difference between highest and lowest temp
+// - How to compute max and min temperatures ?
+// - What's a sensor error ? And what to do ?
+
+// 2) Breaking up into sub-problems
+// - How to ingnore errors ?
+// - Find max value in temp array
+// - Find min value in temp array
+// - Subtract min from max (amplitude) and return it
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(`Max temp is: ${max}`, `Min temp is: ${min}`);
+  return max - min;
+};
+
+console.log(calcTempAmplitude(temperatures));
+
+// Problem 2
+// Function should now receive 2 arrays of temps
+
+// 1) Understanding the problem
+// - With 2 arrays, should we implement functionality twice ? NO! Just two arrays
+
+// 2) Breaking up into sub-problems
+// How to merge 2 arrays ?
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(`Max temp is: ${max}`, `Min temp is: ${min}`);
+  return max - min;
+};
+
+console.log(calcTempAmplitudeNew(temperatures, [3, 5, 50]));
