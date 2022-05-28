54034bddcbb32771c971e7b3e23b6c461a2baf83
kiranbansode <kiran5120135@gmail.com>
2022-05-28 13:35:26 +0530

    5.09. Debugging with the Console and Breakpoints

--- 05/script.js
+++ 05/script.js
@@ -22,23 +22,23 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // - Find min value in temp array
 // - Subtract min from max (amplitude) and return it
 
-const calcTempAmplitude = function (temps) {
-  let max = temps[0];
-  let min = temps[0];
+// const calcTempAmplitude = function (temps) {
+//   let max = temps[0];
+//   let min = temps[0];
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
 
-    if (typeof curTemp !== 'number') continue;
+//     if (typeof curTemp !== 'number') continue;
 
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  console.log(`Max temp is: ${max}`, `Min temp is: ${min}`);
-  return max - min;
-};
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(`Max temp is: ${max}`, `Min temp is: ${min}`);
+//   return max - min;
+// };
 
-console.log(calcTempAmplitude(temperatures));
+// console.log(calcTempAmplitude(temperatures));
 
 // Problem 2
 // Function should now receive 2 arrays of temps
@@ -49,11 +49,54 @@ console.log(calcTempAmplitude(temperatures));
 // 2) Breaking up into sub-problems
 // How to merge 2 arrays ?
 
+// const calcTempAmplitudeNew = function (t1, t2) {
+//   const temps = t1.concat(t2);
+
+//   let max = temps[0];
+//   let min = temps[0];
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+
+//     if (typeof curTemp !== 'number') continue;
+
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(`Max temp is: ${max}`, `Min temp is: ${min}`);
+//   return max - min;
+// };
+
+// console.log(calcTempAmplitudeNew(temperatures, [3, 5, 50]));
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    // C) FIX
+    value: 10,
+  };
+
+  // B) FIND THE BUG
+  console.table(measurement);
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 237;
+  return kelvin;
+};
+
+// A) IDENTIFY
+console.log(measureKelvin());
+
+// USING a DEBUGGER
 const calcTempAmplitudeNew = function (t1, t2) {
   const temps = t1.concat(t2);
 
-  let max = temps[0];
-  let min = temps[0];
+  let max = 0;
+  let min = 0;
 
   for (let i = 0; i < temps.length; i++) {
     const curTemp = temps[i];
@@ -67,4 +110,5 @@ const calcTempAmplitudeNew = function (t1, t2) {
   return max - min;
 };
 
-console.log(calcTempAmplitudeNew(temperatures, [3, 5, 50]));
+// A) Identify
+console.log(calcTempAmplitudeNew([9, 4, 5], [3, 5, 1]));
