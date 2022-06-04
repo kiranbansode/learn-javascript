c494f77b9b3c8fca22e726c6bdf51267f5ce0e77
kiranbansode <kiran5120135@gmail.com>
2022-06-04 15:35:46 +0530

    7.07. Implementing the Game Logic

--- 07/script.js
+++ 07/script.js
@@ -9,9 +9,10 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const x = function () {
-    console.log(23);
-};
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -20,5 +21,25 @@ document.querySelector('.check').addEventListener('click', function () {
 
     if (!guess) {
         document.querySelector('.message').textContent = '⛔ No Number';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct Number';
+    } else if (guess > secretNumber) {
+        if (score < 1) {
+            document.querySelector('.message').textContent =
+                '☠️ You lost the Game!';
+            return;
+        }
+        document.querySelector('.message').textContent = '📈 Too High!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    } else if (guess < secretNumber) {
+        if (score < 1) {
+            document.querySelector('.message').textContent =
+                '☠️ You lost the Game!';
+            return;
+        }
+        document.querySelector('.message').textContent = '📉 Too Low!';
+        score--;
+        document.querySelector('.score').textContent = score;
     }
 });
