e700ae37f3b7f728730653e06f81ced543931687
kiranbansode <kiran5120135@gmail.com>
2022-06-14 21:15:36 +0530

    7.13. Working With Classes

--- 07/modal_window/script.js
+++ 07/modal_window/script.js
@@ -4,8 +4,22 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].textContent);
+    // Show Modal
+    btnsOpenModal[i].addEventListener('click', openModal);
+
+    // Hide Modal
+    btnCloseModal.addEventListener('click', closeModal);
+    overlay.addEventListener('click', closeModal);
 }
