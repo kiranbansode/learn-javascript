37d4a3d5be23c8654078fd74ff0616487ab69258
kiranbansode <kiran5120135@gmail.com>
2022-06-20 20:26:08 +0530

    7.18. Holding Current Score

--- 07/pig_game/script.js
+++ 07/pig_game/script.js
@@ -21,12 +21,23 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+};
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
+    if (!playing) {
+        return;
+    }
+
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice);
 
     // 2. Display dice
     diceEl.classList.remove('hidden');
@@ -40,9 +51,35 @@ btnRoll.addEventListener('click', function () {
             currentScore;
     } else {
         // switch to next player
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
+        switchPlayer();
+    }
+});
+
+btnHold.addEventListener('click', function () {
+    if (!playing) {
+        return;
+    }
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore;
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+        scores[activePlayer];
+
+    // 2. Check if player's score is  >= 100
+    if (scores[activePlayer] >= 20) {
+        // Finish the Game
+        playing = false;
+        diceEl.classList.add('hidden');
+        document
+            .querySelector(`.player--${activePlayer}`)
+            .classList.add('player--winner');
+
+        document
+            .querySelector(`.player--${activePlayer}`)
+            .classList.remove('player--active');
+    } else {
+        // Swtich to next player
+        switchPlayer();
     }
 });
