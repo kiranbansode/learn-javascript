e407f1f7311f7ed0e912680650af628d0121ad75
kiranbansode <kiran5120135@gmail.com>
2022-10-20 20:33:13 +0530

    8.11. The this Keyword in Practice

--- 08/script.js
+++ 08/script.js
@@ -8,14 +8,14 @@
 // 		console.log(output);
 
 // 		if (birthYear >= 1981 && birthYear <= 1996) {
-// 			var millenial = true;
+// 			var millennial = true;
 // 			// Creating New Variable with same name as outer scope's variable
 // 			const firstName = "Meera";
 
 // 			// Reassigning outer scope's variable
 // 			output = "New Output";
 
-// 			const str = `Oh, and you'are a millenial, ${firstName}`;
+// 			const str = `Oh, and you'are a millennial, ${firstName}`;
 // 			console.log(str);
 
 // 			function add(a, b) {
@@ -23,7 +23,7 @@
 // 			}
 // 		}
 // 		// console.log(str);
-// 		console.log(millenial);
+// 		console.log(millennial);
 // 		// console.log(add(2, 3));
 // 		console.log(output);
 // 	}
@@ -38,46 +38,88 @@
 // console.log(age);
 // printAge();
 
-// Variables
-console.log(me);
-// console.log(job);
-// console.log(year);
+// // Variables
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
 
-var me = "Kiran";
-let job = "teacher";
-const year = 1995;
+// var me = "Kiran";
+// let job = "teacher";
+// const year = 1995;
 
-// Functions
-console.log("addDoc1: ", addDoc1(2, 3));
-// console.log("addDoc2: ", addDoc2(2, 3));
-// console.log("addDoc3: ", addDoc3(2, 3));
+// // Functions
+// console.log("addDoc1: ", addDoc1(2, 3));
+// // console.log("addDoc2: ", addDoc2(2, 3));
+// // console.log("addDoc3: ", addDoc3(2, 3));
 
-function addDoc1(a, b) {
-	return a + b;
-}
+// function addDoc1(a, b) {
+// 	return a + b;
+// }
 
-const addDoc2 = function (a, b) {
-	return a + b;
+// const addDoc2 = function (a, b) {
+// 	return a + b;
+// };
+
+// var addDoc3 = (a, b) => {
+// 	return a + b;
+// };
+
+// // Example
+// console.log("numProducts", numProducts);
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+// 	console.log("All products deleted");
+// }
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
+
+console.log(this); // window
+
+const calcAge = function (birthYear) {
+	console.log(2022 - birthYear);
+	console.log(this); // undefined in 'strict' mode
 };
 
-var addDoc3 = (a, b) => {
-	return a + b;
+calcAge(1995);
+
+const calcAgeArrow = (birthYear) => {
+	console.log(2022 - birthYear);
+	console.log(this); // window object
+	// Arrow function will use their parent's scope
 };
 
-// Example
-console.log("numProducts", numProducts);
-if (!numProducts) deleteShoppingCart();
+calcAgeArrow(1995);
 
-var numProducts = 10;
+const kiran = {
+	birthYear: 1995,
+	calcAge: function () {
+		console.log(this); // object where it is defined
+		console.log(2022 - this.birthYear);
+	},
+	calcAge2: () => {
+		console.log(this); // it will return window object
+		console.log(2022 - this.birthYear); // will not work
+	},
+};
 
-function deleteShoppingCart() {
-	console.log("All products deleted");
-}
+kiran.calcAge();
+kiran.calcAge2();
 
-var x = 1;
-let y = 2;
-const z = 3;
+const meera = {
+	year: 2017,
+};
 
-console.log(x === window.x);
-console.log(y === window.y);
-console.log(z === window.z);
+meera.calcAge = kiran.calcAge;
+meera.calcAge();
+
+const f = kiran.calcAge;
+f();
