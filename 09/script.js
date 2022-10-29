6b6dc4b08dfd901db641240398dd5e69a0c1d573
kiranbansode <kiran5120135@gmail.com>
2022-10-29 15:27:44 +0530

    9.13. Optional Chaining (?.)

--- 09/script.js
+++ 09/script.js
@@ -53,6 +53,31 @@ const restaurant = {
 	},
 };
 
+if (restaurant.openingHours.mon && restaurant.openingHours.mon)
+	console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon.open);
+
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+for (const day of days) {
+	const open = restaurant.openingHours[day]?.open ?? "closed";
+	console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? "Method does not exists");
+console.log(restaurant.orderRisotto?.(0, 1) ?? "Method does not exists");
+
+// Arrays
+const users = [{ name: "Kiran", email: "kiran5120135@gmail.com" }];
+const users2 = [];
+
+console.log(users2[0]?.name ?? "User array empty");
+
 // const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 // for (const item of menu) console.log(item);
