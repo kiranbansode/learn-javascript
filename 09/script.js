904cf4da9467b976c78e8645cd95e49de03c45b4
kiranbansode <kiran5120135@gmail.com>
2022-12-03 17:01:59 +0530

    9.18. Maps Iteration

--- 09/script.js
+++ 09/script.js
@@ -381,31 +381,66 @@ for (const [day, { open, close }] of entries) {
 
 // console.log(new Set("KiranBansode").size);
 
-const rest = new Map();
-rest.set("name", "Classico Italiano");
-rest.set(1, "Firenze, Italy");
-console.log(rest.set(2, "Lisbon, Portugal"));
+// const rest = new Map();
+// rest.set("name", "Classico Italiano");
+// rest.set(1, "Firenze, Italy");
+// console.log(rest.set(2, "Lisbon, Portugal"));
 
-rest
-	.set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
-	.set("open", 11)
-	.set("close", 23)
-	.set(true, "We are open :D")
-	.set(false, "We are closed :(");
+// rest
+// 	.set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+// 	.set("open", 11)
+// 	.set("close", 23)
+// 	.set(true, "We are open :D")
+// 	.set(false, "We are closed :(");
 
-console.log(rest.get("name"));
-console.log(rest.get(true));
-console.log(rest.get(1));
+// console.log(rest.get("name"));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
 
-const time = 21;
-console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+// const time = 21;
+// console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
 
-console.log(rest.has("categories"));
-console.log(rest.delete(2));
-// rest.clear();
-const arr = [1, 2];
-rest.set(arr, "test");
-rest.set(document.querySelector("h1"), "Heading");
-console.log(rest);
-console.log(rest.get(arr));
-console.log(rest.size);
+// console.log(rest.has("categories"));
+// console.log(rest.delete(2));
+// // rest.clear();
+// const arr = [1, 2];
+// rest.set(arr, "test");
+// rest.set(document.querySelector("h1"), "Heading");
+// console.log(rest);
+// console.log(rest.get(arr));
+// console.log(rest.size);
+
+const question = new Map([
+	["question", "What is the best programming language in the world ?"],
+	[1, "C"],
+	[2, "C++"],
+	[3, "Java"],
+	[4, "JavaScript"],
+	["correct", 4],
+	[true, "Correct"],
+	[false, "Try Again!"],
+]);
+
+console.log(question);
+
+// Convert object to Map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get("question"));
+
+for (const [key, value] of question) {
+	if (typeof key === "number") console.log(`Option ${key}: ${value}`);
+}
+const answer = Number(prompt("Your answer"));
+console.log(answer);
+
+console.log(question.get(question.get("correct") === answer));
+
+// Convert Map to Array
+
+console.log([...question]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
