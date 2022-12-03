a245802bfceeba7a882ce7a30a336347bbd92f9b
kiranbansode <kiran5120135@gmail.com>
2022-12-03 16:50:06 +0530

    9.17. Maps Fundamentals

--- 09/script.js
+++ 09/script.js
@@ -347,36 +347,65 @@ for (const [day, { open, close }] of entries) {
 // const [p = 1, q = 1, r = 1] = [8, 9];
 // console.log(p, q, r);
 
-const ordersSet = new Set([
-	"Pasta",
-	"Pizza",
-	"Pizza",
-	"Risotto",
-	"Pasta",
-	"Pizza",
-]);
-console.log(ordersSet);
+// const ordersSet = new Set([
+// 	"Pasta",
+// 	"Pizza",
+// 	"Pizza",
+// 	"Risotto",
+// 	"Pasta",
+// 	"Pizza",
+// ]);
+// console.log(ordersSet);
 
-console.log(new Set("Kiran"));
-console.log(ordersSet.size);
-console.log(ordersSet.has("Pizza"));
-console.log(ordersSet.has("Bread"));
-ordersSet.add("Garlic Bread");
-ordersSet.add("Garlic Bread");
-ordersSet.delete("Risotto");
-// ordersSet.clear();
-console.log(ordersSet);
+// console.log(new Set("Kiran"));
+// console.log(ordersSet.size);
+// console.log(ordersSet.has("Pizza"));
+// console.log(ordersSet.has("Bread"));
+// ordersSet.add("Garlic Bread");
+// ordersSet.add("Garlic Bread");
+// ordersSet.delete("Risotto");
+// // ordersSet.clear();
+// console.log(ordersSet);
 
-for (const order of ordersSet) console.log(order);
+// for (const order of ordersSet) console.log(order);
 
-// Examples
+// // Examples
 
-const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
-const staffUnique = [...new Set(staff)];
-console.log(staffUnique);
+// const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
+// const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
 
-console.log(
-	new Set(["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"]).size
-);
+// console.log(
+// 	new Set(["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"]).size
+// );
 
-console.log(new Set("KiranBansode").size);
+// console.log(new Set("KiranBansode").size);
+
+const rest = new Map();
+rest.set("name", "Classico Italiano");
+rest.set(1, "Firenze, Italy");
+console.log(rest.set(2, "Lisbon, Portugal"));
+
+rest
+	.set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+	.set("open", 11)
+	.set("close", 23)
+	.set(true, "We are open :D")
+	.set(false, "We are closed :(");
+
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+console.log(rest.has("categories"));
+console.log(rest.delete(2));
+// rest.clear();
+const arr = [1, 2];
+rest.set(arr, "test");
+rest.set(document.querySelector("h1"), "Heading");
+console.log(rest);
+console.log(rest.get(arr));
+console.log(rest.size);
