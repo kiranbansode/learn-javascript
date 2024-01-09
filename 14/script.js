e10640688d4a64959684a87d306bc8cf2ac2ec48
Kiran Bansode <kiran5120135@gmail.com>
2024-01-09 22:57:27 +0530

    14.05. Prototypes

--- 14/script.js
+++ 14/script.js
@@ -13,13 +13,36 @@ const Person = function (firstName, birthYear) {
 const kiran = new Person("Kiran", 1995);
 console.log(kiran);
 
-// 1. {} New Empty Object is created
+// 1. {} => New Empty Object is created
 // 2. function is called, 'this' gets assign to => {} Newly created empty object
 // 3. {} object is linked to prototype
 // 4. function automatically return {}
 
-const meera = new Person("Meera", 2001);
+const meera = new Person("Meera", 2000);
 const jack = new Person("Jack", 1975);
 console.log(meera, jack);
 
 console.log(kiran instanceof Person);
+
+console.log(Person.prototype);
+
+// Prototypes
+Person.prototype.calcAge = function () {
+	console.log(2024 - this.birthYear);
+};
+
+kiran.calcAge();
+meera.calcAge();
+
+console.log(kiran.__proto__);
+console.log(kiran.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(kiran));
+console.log(Person.prototype.isPrototypeOf(meera));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = "Homo Sapiens";
+console.log(kiran.species, meera);
+
+console.log(kiran.hasOwnProperty("firstName"));
+console.log(kiran.hasOwnProperty("species"));
