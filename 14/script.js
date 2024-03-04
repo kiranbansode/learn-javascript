b14af5e0faa6b1bb4c6d2f0119b4da635339c3c4
Kiran Bansode <kiran5120135@gmail.com>
2024-03-04 20:50:06 +0530

    14.08. - Coding Challenge 1

--- 14/script.js
+++ 14/script.js
@@ -1,74 +1,99 @@
-"use strict";
+// "use strict";
 
-const Person = function (firstName, birthYear) {
-	this.firstName = firstName;
-	this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+// 	this.firstName = firstName;
+// 	this.birthYear = birthYear;
 
-	// Never do this
-	// this.calcAge = function () {
-	// 	console.log(2037 - this.birthYear);
-	// };
+// 	// Never do this
+// 	// this.calcAge = function () {
+// 	// 	console.log(2037 - this.birthYear);
+// 	// };
+// };
+
+// const kiran = new Person("Kiran", 1995);
+// console.log(kiran);
+
+// // 1. {} => New Empty Object is created
+// // 2. function is called, 'this' gets assign to => {} Newly created empty object
+// // 3. {} object is linked to prototype
+// // 4. function automatically return {}
+
+// const meera = new Person("Meera", 2000);
+// const jack = new Person("Jack", 1975);
+// console.log(meera, jack);
+
+// console.log(kiran instanceof Person);
+
+// console.log(Person.prototype);
+
+// // Prototypes
+// Person.prototype.calcAge = function () {
+// 	console.log(2024 - this.birthYear);
+// };
+
+// kiran.calcAge();
+// meera.calcAge();
+
+// console.log(kiran.__proto__);
+// console.log(kiran.__proto__ === Person.prototype);
+
+// console.log(Person.prototype.isPrototypeOf(kiran));
+// console.log(Person.prototype.isPrototypeOf(meera));
+// console.log(Person.prototype.isPrototypeOf(Person));
+
+// Person.prototype.species = "Homo Sapiens";
+// console.log(kiran.species, meera);
+
+// console.log(kiran.hasOwnProperty("firstName"));
+// console.log(kiran.hasOwnProperty("species"));
+
+// console.log(kiran.__proto__);
+// console.log(kiran.__proto__.__proto__);
+// console.log(kiran.__proto__.__proto__.__proto__);
+
+// console.log(Person.prototype.constructor);
+
+// const arr = [3, 5, 6, 7, 4, 5, 9, 3, 3]; // new Array === []
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
+
+// console.log(arr.__proto__.__proto__);
+
+// // Not recommended
+// // may be in future JavaScript relese new method with same name
+// Array.prototype.unique = function () {
+// 	return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+
+// const h1 = document.querySelector("h1");
+// // Prototype Chain
+// // Object => EventTarget => Node => Element => HTMLElement => HTMLHeadingElement
+
+// console.dir((x) => x + 1);
+
+/* --------------------------- Coding Challenge #1 -------------------------- */
+
+const Car = function (make, speed) {
+	this.make = make;
+	this.speed = speed;
 };
 
-const kiran = new Person("Kiran", 1995);
-console.log(kiran);
-
-// 1. {} => New Empty Object is created
-// 2. function is called, 'this' gets assign to => {} Newly created empty object
-// 3. {} object is linked to prototype
-// 4. function automatically return {}
-
-const meera = new Person("Meera", 2000);
-const jack = new Person("Jack", 1975);
-console.log(meera, jack);
-
-console.log(kiran instanceof Person);
-
-console.log(Person.prototype);
-
-// Prototypes
-Person.prototype.calcAge = function () {
-	console.log(2024 - this.birthYear);
+Car.prototype.accelerate = function () {
+	this.speed += 10;
+	console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
-kiran.calcAge();
-meera.calcAge();
-
-console.log(kiran.__proto__);
-console.log(kiran.__proto__ === Person.prototype);
-
-console.log(Person.prototype.isPrototypeOf(kiran));
-console.log(Person.prototype.isPrototypeOf(meera));
-console.log(Person.prototype.isPrototypeOf(Person));
-
-Person.prototype.species = "Homo Sapiens";
-console.log(kiran.species, meera);
-
-console.log(kiran.hasOwnProperty("firstName"));
-console.log(kiran.hasOwnProperty("species"));
-
-console.log(kiran.__proto__);
-console.log(kiran.__proto__.__proto__);
-console.log(kiran.__proto__.__proto__.__proto__);
-
-console.log(Person.prototype.constructor);
-
-const arr = [3, 5, 6, 7, 4, 5, 9, 3, 3]; // new Array === []
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype);
-
-console.log(arr.__proto__.__proto__);
-
-// Not recommended
-// may be in future JavaScript relese new method with same name
-Array.prototype.unique = function () {
-	return [...new Set(this)];
+Car.prototype.brake = function () {
+	this.speed -= 5;
+	console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
-console.log(arr.unique());
+const bmw = new Car("BMW", 120);
+const mercedes = new Car("Mercedes", 95);
 
-const h1 = document.querySelector("h1");
-// Prototype Chain
-// Object => EventTarget => Node => Element => HTMLElement => HTMLHeadingElement
-
-console.dir((x) => x + 1);
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
