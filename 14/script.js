7de6c79b268163cbcad15a09664109cff7d70148
Kiran Bansode <kiran5120135@gmail.com>
2024-03-08 13:58:39 +0530

    14.18. Encapsulation Private Class Fields and Methods

--- 14/script.js
+++ 14/script.js
@@ -406,40 +406,59 @@
 // jay.introduce();
 // jay.calcAge();
 
+// 1.Public fields
+// 2.Private fields
+// 3.Public methods
+// 4.Private methods
+// (thre is also the static version)
+
 class Account {
+	// 1. Public fields (instances)
+	locale = navigator.language;
+
+	// 2. Private fields (instances)
+	#movements = [];
+	#pin;
+
 	constructor(owner, currency, pin) {
 		this.owner = owner;
 		this.currency = currency;
 		// protected property
-		this._pin = pin;
-		this._movements = [];
-		this.locale = navigator.language;
+		this.#pin = pin;
+		// this._movements = [];
+		// this.locale = navigator.language;
 
 		console.log(`Thanks for opening an acocunt, ${owner}`);
 	}
 
+	// 3. Public methods
 	// Public interface
 	getMovements() {
-		return this._movements;
+		return this.#movements;
 	}
 
 	deposit(val) {
-		this._movements.push(val);
+		this.#movements.push(val);
 	}
 
 	withdraw(val) {
 		this.deposit(-val);
 	}
 
-	_approveLoan(val) {
-		return true;
-	}
-
 	requestLoan(val) {
-		if (this._approveLoan(val)) {
+		if (this.#approveLoan(val)) {
 			this.deposit(val);
 			console.log("Loan approved");
 		}
+	}
+
+	static helper() {
+		console.log("Helper");
+	}
+
+	// 4. Private Methods
+	#approveLoan(val) {
+		return true;
 	}
 }
 
@@ -448,8 +467,13 @@ const acc1 = new Account("Jonas", "EUR", 1111);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
 console.log(acc1.getMovements());
 
 console.log(acc1);
-console.log(acc1.pin);
+
+// acc1.#approveLoan(1000);
+// console.log(acc1.#pin);
+// console.log(acc1.#movements);
+
+Account.helper();
+// acc1.helper(); // Not Work
