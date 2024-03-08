ddedec9e7cffe7043480683b89935f7e29d6c17c
Kiran Bansode <kiran5120135@gmail.com>
2024-03-08 13:40:00 +0530

    14.17. Encapsulation Protected Properties and Methods

--- 14/script.js
+++ 14/script.js
@@ -410,28 +410,33 @@ class Account {
 	constructor(owner, currency, pin) {
 		this.owner = owner;
 		this.currency = currency;
-		this.pin = pin;
-		this.movements = [];
+		// protected property
+		this._pin = pin;
+		this._movements = [];
 		this.locale = navigator.language;
 
 		console.log(`Thanks for opening an acocunt, ${owner}`);
 	}
 
 	// Public interface
+	getMovements() {
+		return this._movements;
+	}
+
 	deposit(val) {
-		this.movements.push(val);
+		this._movements.push(val);
 	}
 
 	withdraw(val) {
 		this.deposit(-val);
 	}
 
-	approveLoan(val) {
+	_approveLoan(val) {
 		return true;
 	}
 
 	requestLoan(val) {
-		if (this.approveLoan(val)) {
+		if (this._approveLoan(val)) {
 			this.deposit(val);
 			console.log("Loan approved");
 		}
@@ -444,6 +449,7 @@ acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
 acc1.approveLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
 console.log(acc1.pin);
