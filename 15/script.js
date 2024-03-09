c37f2228dc01e51fd4a55dc122cf5fee3f81c80c
Kiran Bansode <kiran5120135@gmail.com>
2024-03-09 22:13:53 +0530

    15.13. Move to Marker On Click

--- 15/script.js
+++ 15/script.js
@@ -3,6 +3,7 @@
 class Workout {
 	date = new Date();
 	id = (Date.now() + "").slice(-10);
+	clicks = 0;
 
 	constructor(coords, distance, duration) {
 		this.coords = coords; // [lat, lng]
@@ -17,6 +18,10 @@ class Workout {
 		this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
 			months[this.date.getMonth()]
 		} ${this.date.getDate()}`;
+	}
+
+	click() {
+		this.clicks++;
 	}
 }
 
@@ -67,6 +72,7 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 class App {
 	#map;
+	#mapZoomLevel = 13;
 	#mapEvent;
 	#workouts = [];
 
@@ -74,6 +80,7 @@ class App {
 		this._getPosition();
 		form.addEventListener("submit", this._newWorkout.bind(this));
 		inputType.addEventListener("change", this._toggleElevationField);
+		containerWorkouts.addEventListener("click", this._moveToPopup.bind(this));
 	}
 
 	_getPosition() {
@@ -87,7 +94,7 @@ class App {
 		const { latitude, longitude } = position.coords;
 		console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-		this.#map = L.map("map").setView([latitude, longitude], 13);
+		this.#map = L.map("map").setView([latitude, longitude], this.#mapZoomLevel);
 		// console.log(map);
 
 		L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
@@ -226,6 +233,26 @@ class App {
   	  </li>`;
 
 		form.insertAdjacentHTML("afterend", html);
+	}
+
+	_moveToPopup(e) {
+		const workoutEl = e.target.closest(".workout");
+		console.log(workoutEl);
+
+		if (!workoutEl) return;
+
+		const workout = this.#workouts.find((work) => work.id === workoutEl.dataset.id);
+		console.log(workout);
+
+		this.#map.setView(workout.coords, this.#mapZoomLevel, {
+			animate: true,
+			pan: {
+				duration: 1,
+			},
+		});
+
+		// using the public interface
+		workout.click();
 	}
 }
 
