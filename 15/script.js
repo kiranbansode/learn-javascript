94005882cc6882b07cea89927fd500865fe826ec
Kiran Bansode <kiran5120135@gmail.com>
2024-03-09 23:20:16 +0530

    15.14. Working with localStorage

--- 15/script.js
+++ 15/script.js
@@ -77,7 +77,12 @@ class App {
 	#workouts = [];
 
 	constructor() {
+		// Get user's position
 		this._getPosition();
+
+		// Get data from local storage
+		this._getLocalStorage();
+
 		form.addEventListener("submit", this._newWorkout.bind(this));
 		inputType.addEventListener("change", this._toggleElevationField);
 		containerWorkouts.addEventListener("click", this._moveToPopup.bind(this));
@@ -92,10 +97,8 @@ class App {
 
 	_loadMap(position) {
 		const { latitude, longitude } = position.coords;
-		console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
 		this.#map = L.map("map").setView([latitude, longitude], this.#mapZoomLevel);
-		// console.log(map);
 
 		L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
 			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -103,6 +106,10 @@ class App {
 
 		// Handling clicks on map
 		this.#map.on("click", this._showForm.bind(this));
+
+		this.#workouts.forEach((work) => {
+			this._renderWorkoutMarker(work);
+		});
 	}
 
 	_showForm(mapE) {
@@ -161,7 +168,6 @@ class App {
 
 		// Add new object to workout array
 		this.#workouts.push(workout);
-		console.log(workout);
 
 		// render workout on may as marker
 		this._renderWorkoutMarker(workout);
@@ -171,6 +177,9 @@ class App {
 
 		// hide form  + clear input fields
 		this._hideForm();
+
+		// Set local storage to all workoutsa
+		this._setLocalStorage();
 	}
 
 	_renderWorkoutMarker(workout) {
@@ -237,12 +246,10 @@ class App {
 
 	_moveToPopup(e) {
 		const workoutEl = e.target.closest(".workout");
-		console.log(workoutEl);
 
 		if (!workoutEl) return;
 
 		const workout = this.#workouts.find((work) => work.id === workoutEl.dataset.id);
-		console.log(workout);
 
 		this.#map.setView(workout.coords, this.#mapZoomLevel, {
 			animate: true,
@@ -252,7 +259,27 @@ class App {
 		});
 
 		// using the public interface
-		workout.click();
+		// workout.click();
+	}
+
+	_setLocalStorage() {
+		localStorage.setItem("workouts", JSON.stringify(this.#workouts));
+	}
+
+	_getLocalStorage() {
+		const data = JSON.parse(localStorage.getItem("workouts"));
+
+		if (!data) return;
+
+		this.#workouts = data;
+		this.#workouts.forEach((work) => {
+			this._renderWorkout(work);
+		});
+	}
+
+	reset() {
+		localStorage.removeItem("workouts");
+		location.reload();
 	}
 }
 
