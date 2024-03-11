fe321d68de797da76d11d57070e3b65b42842838
Kiran Bansode <kiran5120135@gmail.com>
2024-03-11 20:58:28 +0530

    16.06. Promises and the Fetch API

--- 16/script.js
+++ 16/script.js
@@ -20,48 +20,55 @@ const renderCountry = function (data, className = "") {
 	countriesContainer.style.opacity = "1";
 };
 
-const getCountryAndNeighbour = function (country) {
-	const request = new XMLHttpRequest();
-	request.open("GET", `https://restcountries.com/v2/name/${country}`);
-	request.send();
+// const getCountryAndNeighbour = function (country) {
+// 	const request = new XMLHttpRequest();
+// 	request.open("GET", `https://restcountries.com/v2/name/${country}`);
+// 	request.send();
 
-	request.addEventListener("load", function () {
-		const [data] = JSON.parse(this.responseText);
-		console.log(data);
+// 	request.addEventListener("load", function () {
+// 		const [data] = JSON.parse(this.responseText);
+// 		console.log(data);
 
-		// Rendere Country 1
-		renderCountry(data);
+// 		// Rendere Country 1
+// 		renderCountry(data);
 
-		// Get neighbour country (2)
-		const neighbour = data.borders?.[0];
+// 		// Get neighbour country (2)
+// 		const neighbour = data.borders?.[0];
 
-		if (!neighbour) return;
+// 		if (!neighbour) return;
 
-		const request2 = new XMLHttpRequest();
-		request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
-		request2.send();
+// 		const request2 = new XMLHttpRequest();
+// 		request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
+// 		request2.send();
 
-		request2.addEventListener("load", function () {
-			const data2 = JSON.parse(this.responseText);
-			console.log(data2);
-			renderCountry(data2, "neighbour");
-		});
-	});
-};
+// 		request2.addEventListener("load", function () {
+// 			const data2 = JSON.parse(this.responseText);
+// 			console.log(data2);
+// 			renderCountry(data2, "neighbour");
+// 		});
+// 	});
+// };
 
-// getCountryAndNeighbour("portugal");
-getCountryAndNeighbour("bharat");
-// getCountryAndNeighbour("usa");
+// // getCountryAndNeighbour("portugal");
+// getCountryAndNeighbour("bharat");
+// // getCountryAndNeighbour("usa");
 
-setTimeout(() => {
-	console.log(`1 second passed`);
-	setTimeout(() => {
-		console.log(`2 second passed`);
-		setTimeout(() => {
-			console.log(`3 second passed`);
-			setTimeout(() => {
-				console.log(`4 second passed`);
-			}, 1000);
-		}, 1000);
-	}, 1000);
-}, 1000);
+// setTimeout(() => {
+// 	console.log(`1 second passed`);
+// 	setTimeout(() => {
+// 		console.log(`2 second passed`);
+// 		setTimeout(() => {
+// 			console.log(`3 second passed`);
+// 			setTimeout(() => {
+// 				console.log(`4 second passed`);
+// 			}, 1000);
+// 		}, 1000);
+// 	}, 1000);
+// }, 1000);
+
+// const request = new XMLHttpRequest();
+// request.open("GET", `https://restcountries.com/v2/name/${country}`);
+// request.send();
+
+const request = fetch("https://restcountries.com/v2/name/portugal");
+console.log(request);
