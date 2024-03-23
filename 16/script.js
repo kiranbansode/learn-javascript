48866aaa85072916713df0a34f2284b9207092f9
Kiran Bansode <kiran5120135@gmail.com>
2024-03-23 15:37:23 +0530

    16.13. The Event Loop in Practice

--- 16/script.js
+++ 16/script.js
@@ -1,29 +1,29 @@
 "use strict";
 
-const btn = document.querySelector(".btn-country");
-const countriesContainer = document.querySelector(".countries");
+// const btn = document.querySelector(".btn-country");
+// const countriesContainer = document.querySelector(".countries");
 
-const renderError = function (msg) {
-	countriesContainer.insertAdjacentText("beforeend", msg);
-	countriesContainer.style.opacity = "1";
-};
+// const renderError = function (msg) {
+// 	countriesContainer.insertAdjacentText("beforeend", msg);
+// 	countriesContainer.style.opacity = "1";
+// };
 
-const renderCountry = function (data, className = "") {
-	const html = `
-  <article class="country ${className}">
-  <img class="country__img" src="${data.flag}" />
-  <div class="country__data">
-    <h3 class="country__name">${data.name}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
-    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-  </div>
-</article>`;
+// const renderCountry = function (data, className = "") {
+// 	const html = `
+//   <article class="country ${className}">
+//   <img class="country__img" src="${data.flag}" />
+//   <div class="country__data">
+//     <h3 class="country__name">${data.name}</h3>
+//     <h4 class="country__region">${data.region}</h4>
+//     <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+//     <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//     <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//   </div>
+// </article>`;
 
-	countriesContainer.insertAdjacentHTML("beforeend", html);
-	countriesContainer.style.opacity = "1";
-};
+// 	countriesContainer.insertAdjacentHTML("beforeend", html);
+// 	countriesContainer.style.opacity = "1";
+// };
 
 // const getCountryAndNeighbour = function (country) {
 // 	const request = new XMLHttpRequest();
@@ -83,15 +83,15 @@ const renderCountry = function (data, className = "") {
 // 		});
 // };
 
-const getJSON = function (url, errorMsg = "Something went wrong!") {
-	return fetch(url).then((response) => {
-		if (!response.ok) {
-			throw new Error(`${errorMsg} (${response.status})`);
-		}
+// const getJSON = function (url, errorMsg = "Something went wrong!") {
+// 	return fetch(url).then((response) => {
+// 		if (!response.ok) {
+// 			throw new Error(`${errorMsg} (${response.status})`);
+// 		}
 
-		return response.json();
-	});
-};
+// 		return response.json();
+// 	});
+// };
 
 // const getCountryData = function (country) {
 // 	// Country 1
@@ -160,28 +160,41 @@ const getJSON = function (url, errorMsg = "Something went wrong!") {
 
 // https://geocode.xyz/51.50354,-0.12768?geoit=xml
 
-const whereAmI = function (lat, lng) {
-	fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-		.then((res) => {
-			console.log(res);
-			if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
-			return res.json();
-		})
-		.then((data) => {
-			console.log(data);
-			console.log(`You are in ${data.city}, ${data.country}`);
+// const whereAmI = function (lat, lng) {
+// 	fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+// 		.then((res) => {
+// 			console.log(res);
+// 			if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+// 			return res.json();
+// 		})
+// 		.then((data) => {
+// 			console.log(data);
+// 			console.log(`You are in ${data.city}, ${data.country}`);
 
-			return fetch(`https://restcountries.com/v2/name/${data.country}`);
-		})
-		.then((res) => {
-			if (!res.ok) throw new Error(`Country not found ${res.status}`);
+// 			return fetch(`https://restcountries.com/v2/name/${data.country}`);
+// 		})
+// 		.then((res) => {
+// 			if (!res.ok) throw new Error(`Country not found ${res.status}`);
 
-			return res.json();
-		})
-		.then((data) => renderCountry(data[0]))
-		.catch((err) => console.error(`${err.message}`));
-};
+// 			return res.json();
+// 		})
+// 		.then((data) => renderCountry(data[0]))
+// 		.catch((err) => console.error(`${err.message}`));
+// };
 
-whereAmI(52.508, 13.381);
-whereAmI(19.037, 72.873);
-whereAmI(-33.933, 18.474);
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
+
+console.log("Test Start");
+
+setTimeout(() => {
+	console.log("0 sec timer.");
+}, 0);
+Promise.resolve("Resolved promise 1").then((res) => console.log(res));
+Promise.resolve("Resolved Promise 2").then((res) => {
+	for (let i = 0; i < 10000000000; i++) {}
+	console.log(res);
+});
+
+console.log("Test end");
